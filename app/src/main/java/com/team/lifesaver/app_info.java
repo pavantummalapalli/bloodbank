cb15aaf6368cad6b2bcc5419acb6569cef9ed29b
pavan.t <pavan.t@flipkart.com>
2016-03-01 11:03:21 +0530

    Intial Commit

---
+++ app/src/main/java/com/team/lifesaver/app_info.java
@@ -0,0 +1,35 @@
+package com.team.lifesaver;
+
+import android.graphics.LinearGradient;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import com.team.lifesaver.R;
+
+public class app_info extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.app_info);
+
+        // set title bar
+        LinearLayout app_info_titlebar=(LinearLayout) findViewById(R.id.app_info_titlebar);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.app_info_titlebar);
+
+
+
+
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/team/lifesaver/blooddonate_tips.java
@@ -0,0 +1,45 @@
+package com.team.lifesaver;
+
+import android.content.Intent;
+import android.graphics.LinearGradient;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import com.team.lifesaver.R;
+
+public class blooddonate_tips extends ActionBarActivity {
+
+    TextView titlebartext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blooddonate_tips);
+        //set custom title bar
+        LinearLayout blooddonate_titlebar=(LinearLayout) findViewById(R.id.blooddonate_titlebar);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.blooddonate_titlebar);
+        titlebartext=(TextView) findViewById(R.id.blooddonate_titlebar_text);
+        titlebartext.setText("Tips");
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}
